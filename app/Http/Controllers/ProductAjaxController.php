d02795b7f29b08495b941f765c25d341be1d23b9
Md Rayhan Ali <78335557+rayhan2001@users.noreply.github.com>
2023-03-03 23:54:07 +0600

    first commit

---
+++ app/Http/Controllers/ProductAjaxController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+use DataTables;
+use Illuminate\Http\Request;
+
+class ProductAjaxController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        if ($request->ajax()) {
+            $data = Product::latest()->get();
+            return Datatables::of($data)
+                    ->addIndexColumn()
+                    ->addColumn('action', function($row){
+   
+                           $btn = '<a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Edit" class="edit btn btn-primary btn-sm editProduct">Edit</a>';
+   
+                           $btn = $btn.' <a href="javascript:void(0)" data-toggle="tooltip"  data-id="'.$row->id.'" data-original-title="Delete" class="btn btn-danger btn-sm deleteProduct">Delete</a>';
+    
+                            return $btn;
+                    })
+                    ->rawColumns(['action'])
+                    ->make(true);
+        }
+      
+        return view('productAjax');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Product::updateOrCreate(['id' => $request->product_id],
+                ['name' => $request->name, 'detail' => $request->detail]);        
+   
+        return response()->json(['success'=>'Product saved successfully.']);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        Product::find($id)->delete();
+     
+        return response()->json(['success'=>'Product deleted successfully.']);
+    }
+}
